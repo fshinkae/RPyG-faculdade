61f780cf07bb303685222b6471cbb939fa7d388c
PauloKT <paulohamaral89@gmail.com>
2024-11-09 15:31:21 -0300

    fix: adjust of service methods

--- services/monster_adventure_service.py
+++ services/monster_adventure_service.py
@@ -2,7 +2,7 @@ import random
 
 from services import character_service, monster_service
 
-defense_buff_active = {}
+defense_buff_active = False
 
 
 def roll_d20():
@@ -36,21 +36,21 @@ def dodge_monster(character_info, monster):
     return False
 
 
-def character_attack(character_id, opponent_defense, monster):
+def character_attack(character_info, monster):
     global defense_buff_active
-    character_info = character_service.get_character_by_id(character_id)
+    defense_monster = monster['attributes']['defense']
     if character_info:
         attack = character_info['attributes']['attack']
         dice = roll_d20()
 
         if not dodge_monster(character_info, monster):
-            if dice + attack >= opponent_defense:
+            if dice + attack >= defense_monster:
                 if dice == 20:
-                    damage = attack - opponent_defense
+                    damage = attack - defense_monster
                     damage = damage * 2
                     monster['attributes']['life'] -= damage
                 else:
-                    damage = attack - opponent_defense
+                    damage = attack - defense_monster
                     monster['attributes']['life'] -= damage
                 return monster, damage
             else:
@@ -61,38 +61,41 @@ def character_attack(character_id, opponent_defense, monster):
 def monster_attack(character_id, monster):
     global defense_buff_active
     character_info = character_service.get_character_by_id(character_id)
-    if character_info:
-        defense = character_info['attributes']['defense']
-        dice = roll_d20()
-        attack = dice + monster['attributes']['attack']
+    if not character_info:
+        return "Character not found", 0
 
-        if not dodge_character(character_info, monster):
-            if attack >= defense:
-                if dice == 20:
-                    damage = (monster['attributes']['attack'] * 2) - defense
-                else:
-                    damage = monster['attributes']['attack'] - defense
+    defense = character_info['attributes']['defense']
+    dice = roll_d20()
+    attack = dice + monster['attributes']['attack']
 
-                character_info['attributes']['life'] -= damage
-
-                if defense_buff_active.get(character_id):
-                    character_info['attributes']['defense'] -= 5
-                    character_service.update_character_info(character_id, character_info)
-                    defense_buff_active[character_id] = False
-
-                result = character_service.update_character_attributes(character_id, character_info['attributes'])
-                return result, damage
+    if not dodge_character(character_info, monster):
+        if attack >= defense:
+            if dice == 20:
+                damage = (monster['attributes']['attack'] * 2) - defense
             else:
-                return None, 0
+                damage = monster['attributes']['attack'] - defense
+
+            character_info['attributes']['life'] -= damage
+
+            if defense_buff_active:
+                character_info['attributes']['defense'] -= 5
+                character_service.update_character_attributes(character_id, character_info['attributes'])
+                defense_buff_active = False
+
+            result = character_service.update_character_attributes(character_id, character_info['attributes'])
+            return result, damage
+        else:
+            return None, 0
+    return None, 0
+
+
+def defend_character(character_info, character_id):
+
+    global defense_buff_active
+    if defense_buff_active:
         return None, 0
 
-
-def defend_character(character_id):
-    global defense_buff_active
-    character_info = character_service.get_character_by_id(character_id)
-    if character_info:
-        character_info['attributes']['defense'] += 5
-        character_service.update_character_info(character_id, character_info)
-        defense_buff_active[character_id] = True
-        return character_info, character_info['attributes']['defense']
-    return None
+    character_info['attributes']['defense'] += 5
+    character_service.update_character_attributes(character_id, character_info['attributes'])
+    defense_buff_active = True
+    return character_info, character_info['attributes']['defense']
